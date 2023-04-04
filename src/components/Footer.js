1111ee881aba1557324cad19b39639179b45e0d4
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-04-04 11:02:53 +0530

    Home page is done for Shopping cart App of Fake Crocs Store.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div id="footer">
       <footer>
         <p>Copyright © 2023 Harsha </p>
+        {/* <p>Home page Content, Photos and Video copyrights goeas Crocs Official Website</p> */}
       </footer>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
+import video from "../img/crocsShort.mp4";
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div id="homeDiv">
+      <div id="homeLeft">
+        <div className="content">
+          <h1>GREEN COMES IN EVERY COLOR</h1>
+          <h3>Same Crocs.</h3>
+          <h3>Half the carbon footprint by 2030</h3>
+        </div>
+      </div>
+      <div id="homeRight">
+        <video
+          src={video}
+          width="100%"
+          height="500"
+          autoPlay={true}
+          loop={true}
+          muted={true}
+        />
+      </div>
     </div>
   );
 };
