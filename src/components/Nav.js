bd79731096a9c8014bb50f7ea252f124d317dd46
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-05-02 14:45:30 +0530

    Light and Dark modes.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,47 @@
 import { Link } from "react-router-dom";
 import crocsLogo from "../img/crocsLogo.svg";
 import cartLogo from "../img/cart.svg";
+import dark from "../img/dark.png";
+import light from "../img/light.png";
 import { useState } from "react";
 const Nav = () => {
-  const [activeButton, setActiveButton] = useState(null);
+  // Dark and Light mode activation
+  let [darks, setDarkmode] = useState(dark);
+  let [lightmode, setLightmode] = useState(true);
+  const body = document.querySelector(".body");
+  const changeLogo = () => {
+    console.log("dark");
+    if (lightmode) {
+      setDarkmode(light);
+      setLightmode(false);
+      body.classList.add("darkMode");
+      body.classList.remove("lightMode");
 
-  const handleButtonClick = (color, event) => {
-    if (activeButton) {
-      activeButton.style.backgroundImage = "";
+      console.log(darks);
+      console.log(lightmode);
+    } else {
+      console.log(darks);
+      console.log(lightmode);
+      setLightmode(true);
+      setDarkmode(dark);
+      body.classList.remove("darkMode");
+      body.classList.add("lightMode");
     }
-    event.target.style.backgroundImage = color;
-    setActiveButton(event.target);
+
+    // const [activeButton, setActiveButton] = useState(null);
+
+    // const handleButtonClick = (color, event) => {
+    //   if (activeButton) {
+    //     activeButton.style.backgroundImage = "";
+    //   }
+    //   event.target.style.backgroundImage = color;
+    //   setActiveButton(event.target);
   };
   return (
     <div className="nav">
+      <div className="darkLogo" onClick={() => changeLogo()}>
+        <img src={darks} alt="" />
+      </div>
       <div id="logo">
         <img id="logoImg" src={crocsLogo} alt="" />
         <h1>Fake Crocs Store</h1>
@@ -22,12 +50,12 @@ const Nav = () => {
         <Link to="shopping-cart/">
           <div
             className="navElement"
-            onClick={(event) =>
-              handleButtonClick(
-                "linear-gradient(to right, #579bb1, #4e7c92, #435e71, #354250, #252931, #252931, #252931, #252931, #354250, #435e71, #4e7c92, #579bb1)",
-                event
-              )
-            }
+            // onClick={(event) =>
+            //   handleButtonClick(
+            //     "linear-gradient(to right, #579bb1, #4e7c92, #435e71, #354250, #252931, #252931, #252931, #252931, #354250, #435e71, #4e7c92, #579bb1)",
+            //     event
+            //   )
+            // }
           >
             <li>Home</li>
           </div>
@@ -35,36 +63,36 @@ const Nav = () => {
         <Link to="shopping-cart/shop">
           <div
             className="navElement"
-            onClick={(event) =>
-              handleButtonClick(
-                "linear-gradient(to right, #579bb1, #4e7c92, #435e71, #354250, #252931, #252931, #252931, #252931, #354250, #435e71, #4e7c92, #579bb1)",
-                event
-              )
-            }
+            // onClick={(event) =>
+            //   handleButtonClick(
+            //     "linear-gradient(to right, #579bb1, #4e7c92, #435e71, #354250, #252931, #252931, #252931, #252931, #354250, #435e71, #4e7c92, #579bb1)",
+            //     event
+            //   )
+            // }
           >
             <li>Shop</li>
           </div>
         </Link>
         <Link className="navElement" to="shopping-cart/contact">
           <div
-            onClick={(event) =>
-              handleButtonClick(
-                "linear-gradient(to right, #579bb1, #4e7c92, #435e71, #354250, #252931, #252931, #252931, #252931, #354250, #435e71, #4e7c92, #579bb1)",
-                event
-              )
-            }
+            // onClick={(event) =>
+            //   handleButtonClick(
+            //     "linear-gradient(to right, #579bb1, #4e7c92, #435e71, #354250, #252931, #252931, #252931, #252931, #354250, #435e71, #4e7c92, #579bb1)",
+            //     event
+            //   )
+            // }
           >
             <li>Contact</li>
           </div>
         </Link>
       </ul>
       <div
-        onClick={(event) =>
-          handleButtonClick(
-            "linear-gradient(to right, #579bb1, #7fabc4, #a2bcd4, #c3cfe1, #dfe2ee, #dfe2ee, #dfe2ee, #dfe2ee, #c3cfe1, #a2bcd4, #7fabc4, #579bb1)",
-            event
-          )
-        }
+        // onClick={(event) =>
+        //   handleButtonClick(
+        //     "linear-gradient(to right, #579bb1, #7fabc4, #a2bcd4, #c3cfe1, #dfe2ee, #dfe2ee, #dfe2ee, #dfe2ee, #c3cfe1, #a2bcd4, #7fabc4, #579bb1)",
+        //     event
+        //   )
+        // }
         className="cartMain"
       >
         <Link to="shopping-cart/cart">
